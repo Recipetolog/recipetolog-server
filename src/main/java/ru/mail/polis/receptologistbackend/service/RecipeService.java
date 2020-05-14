4ece65ba9d62318f4ea3fd40d4635f6fb9161246
shakirov-aa <almick17933@gmail.com>
2020-05-14 23:24:26 +0500

    Implement API "/recipes/with-all-these-ingredients"

--- src/main/java/ru/mail/polis/receptologistbackend/service/RecipeService.java
+++ src/main/java/ru/mail/polis/receptologistbackend/service/RecipeService.java
@@ -7,6 +7,8 @@ import ru.mail.polis.receptologistbackend.repository.IngredientRepository;
 import ru.mail.polis.receptologistbackend.repository.RecipeRepository;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 import java.util.TreeSet;
 
@@ -42,6 +44,31 @@ public class RecipeService {
     }
 
     public Set<Recipe> getRecipesWhichContainAllTheseIngredients(String[] ing) {
-        throw new UnsupportedOperationException("Implement me!");
+        Queue<Ingredient> ingredients = new LinkedList<>();
+        for (String i : ing) {
+            Ingredient ingredient = ingredientRepository.findByName(i.toLowerCase());
+            if (ingredient == null) {
+                /*
+                 if one ingredient is null then it means that there are no recipes with all these ingredients
+                 so we return empty set
+                 */
+                return new TreeSet<>();
+            }
+            ingredients.add(ingredient);
+        }
+
+        Set<Long> idsOfRecipes = new HashSet<>();
+        // add ids of recipes of the first ingredient and delete this ingredient
+        idsOfRecipes.addAll(ingredients.poll().getRecipes());
+        for (Ingredient ingredient : ingredients) {
+            idsOfRecipes.retainAll(ingredient.getRecipes()); // intersect
+        }
+
+        Set<Recipe> recipes = new TreeSet<>();
+        for (long l : idsOfRecipes) {
+            recipes.add(recipeRepository.findById(l));
+        }
+
+        return recipes;
     }
 }
