5598bec800f849a9c1050add1e3a7dc52d6bef12
shakirov-aa <almick17933@gmail.com>
2020-05-16 20:17:38 +0500

    Fix id GenerationType in class Recipe

--- src/main/java/ru/mail/polis/receptologistbackend/domain/Recipe.java
+++ src/main/java/ru/mail/polis/receptologistbackend/domain/Recipe.java
@@ -17,7 +17,7 @@ import java.util.Map;
 @Table(name = "recipe")
 public class Recipe implements Serializable, Comparable<Recipe> {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name; // name of recipe
